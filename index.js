5ff0e794ce2a7f6adc88a67186e05ab55bfd6d2f
girl-on-a-string <frenchfries55@protonmail.com>
2024-10-02 12:36:04 -0500

    overlay now shows up on add book click

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 
 const addBook = document.getElementById("add-book");
 const popUp = document.getElementById("popup");
+const overlay = document.getElementById("overlay");
 
 // popup on addbook click
 
 addBook.addEventListener("click", () => {
+    overlay.style.display = "initial";
+
     popUp.style.display = "initial";
     popUp.style.top = "50%";
     popUp.style.bottom = "50%";
