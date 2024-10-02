f29932a6784fdcb23f32685ce0bca8bafafd260f
girl-on-a-string <frenchfries55@protonmail.com>
2024-10-02 11:38:33 -0500

    popup shows up on add book button click, added overlay div

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 //variables
 
 const addBook = document.getElementById("add-book");
+const popUp = document.getElementById("popup");
 
 // popup on addbook click
 
 addBook.addEventListener("click", () => {
-   
+    popUp.style.display = "initial";
+    popUp.style.top = "50%";
+    popUp.style.bottom = "50%";
 })
 
 //
