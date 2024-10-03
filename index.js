c425c493dc427ab8011ba953a8344ff7381a4967
girl-on-a-string <frenchfries55@protonmail.com>
2024-10-03 12:34:42 -0500

    added button to form + preventdefault, created closePopup function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const addBook = document.getElementById("add-book");
 const popUp = document.getElementById("popup");
 const overlay = document.getElementById("overlay");
+const submit = document.getElementById("submit");
 
 // popup on addbook click
 
@@ -12,12 +13,24 @@ addBook.addEventListener("click", () => {
     popUp.style.display = "initial";
     popUp.style.top = "50%";
     popUp.style.bottom = "50%";
-})
+});
 
-overlay.addEventListener("click", () => {
+// make popup disappear when you click outside it
+
+function closePopup () {
     overlay.style.display = "none";
-
     popUp.style.display = "none";
+}
+
+overlay.addEventListener("click", () => { 
+    closePopup();
+});
+
+// submit data + close popup
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault()
+    closePopup()
 });
 
 //
