331849dd0b9693d4b4bc1a7ebc4d5fd5fe6917ea
girl-on-a-string <frenchfries55@protonmail.com>
2024-10-07 13:39:37 -0500

    variables now correctly getting values from inputs accept status

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const addBook = document.getElementById("add-book");
 const popUp = document.getElementById("popup");
 const overlay = document.getElementById("overlay");
 const submit = document.getElementById("submit");
+const bookContainer = document.getElementById("book-list");
 
 // popup on addbook click
 
@@ -29,18 +30,42 @@ overlay.addEventListener("click", () => {
 // submit data + close popup
 
 submit.addEventListener("click", (e) => {
-    e.preventDefault()
-    closePopup()
+    e.preventDefault();
+    closePopup();
+
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let length = document.getElementById("length").value;
+    let status = document.getElementsByClassName("status").value;
+
+    console.log(title, author, length, status);
 });
 
-//
+// book handling
 
 const myLibrary = [];
 
-function book () { //the constructor
+function book (title, author, length, status) { //the object constructor
+    this.title = title;
+    this.author = author;
+    this.length = length;
+    this.status = status;
 
+    this.itemState = function () {
+        console.log(`title: ${title}, author: ${author}, length: ${length}, status: ${status}`);
+    }
 }
 
 function addBookToLibrary () {
 
+}
+
+addBookToLibrary();
+
+// book display
+
+function bookDisplay () {
+    let bookItem = document.createElement("div");
+    bookItem.classList.add("book-item");
+    bookContainer.appendChild(bookItem);
 }
